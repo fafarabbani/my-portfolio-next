0b1173d6bc801ca269b5ea915ad7695e33c11bf1
FafaRabbani <fafa.rabbanii@gmail.com>
2025-05-31 09:18:51 +0700

    update resume and linkedin

--- components/intro.tsx
+++ components/intro.tsx
@@ -90,7 +90,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/cv/resume.pdf"
+          href="/cv/resume-izzuddin.pdf"
           download
         >
           Download CV{" "}
@@ -107,7 +107,7 @@ export default function Intro() {
       >
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/m-izzuddin-ar-rabbani/"
+          href="https://www.linkedin.com/in/mizzuddinfar-rabbani/"
           target="_blank"
         >
           <BsLinkedin />
